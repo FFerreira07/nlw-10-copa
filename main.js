59803ddf762f4f0b4a2c1436ca80b47b501fd5af
Felipe Ferreira <felipeferreira2412@gmail.com>
2022-11-04 13:02:59 -0300

    add readme.md

--- main.js
+++ main.js
@@ -1,14 +1,4 @@
 let delay = -0.4;
-
-document.querySelector('#cards').innerHTML =
- 
-    creatCard('24/11', 'QUINTA', creatGame("brazil","16:00","serbia") + creatGame("portugal","13:00","ghana") + creatGame("uruguay","10:00","southKorea"))
-    +
-    creatCard('28/11', 'SEGUNDA', creatGame("brazil","13:00","switzerland") + creatGame("portugal","16:00","uruguay") + creatGame("cameroon","07:00","serbia"))
-    +
-    creatCard('02/12', 'SEXTA', creatGame("cameroon","16:00","brazil") + creatGame("serbia","16:00","switzerland") + creatGame("southKorea","12:00","portugal"));
-
-    
 function creatCard(date, day, game){
   delay+= 0.4;
   return `
@@ -20,7 +10,6 @@ function creatCard(date, day, game){
   </div>
   `
 }
-
 function creatGame(player1, hour, player2){
   return`
     <li>
@@ -30,4 +19,11 @@ function creatGame(player1, hour, player2){
     </li>
   `
 }
+document.querySelector('#cards').innerHTML =
+ 
+    creatCard('24/11', 'QUINTA', creatGame("brazil","16:00","serbia") + creatGame("portugal","13:00","ghana") + creatGame("uruguay","10:00","southKorea"))
+    +
+    creatCard('28/11', 'SEGUNDA', creatGame("brazil","13:00","switzerland") + creatGame("portugal","16:00","uruguay") + creatGame("cameroon","07:00","serbia"))
+    
+
 
